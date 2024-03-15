5c6253cd1ea1bb0357bb2d8af79163adbd607011
Manders93 <asedgwick2@my.wctc.edu>
2024-03-14 20:40:31 -0500

    Add project files.

---
+++ RaceTrack/Cars/HyundaiSonata.cs
@@ -0,0 +1,34 @@
+﻿using RaceTrack.RaceTrack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class HyundaiSonata : RaceCar
+    {
+        public HyundaiSonata()
+        {
+            Name = "Hyundai Sonata";
+            TopSpeed = 95;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} starts up like a champ!");
+        }
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} is braking");
+        }
+
+        //PositionCars();
+        //DriversReady();
+        //StartRace();
+        //EndRace();
+        //AnnounceWinners();
+
+    }
+}
---
+++ RaceTrack/Drivers/Amanda.cs
@@ -0,0 +1,24 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Amanda : Driver
+    {
+        public Amanda(RaceCar car) : base(car)
+        {
+            Name = "Amanda";
+            SkillLevel = 10;
+        }
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
+
